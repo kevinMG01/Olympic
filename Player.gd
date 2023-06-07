ece988faab13b3d44da154f257179cc71bf483df
kevinMG01 <montanokevin46@gmail.com>
2023-06-06 21:02:15 -0300

    assa
    asa

--- Player.gd
+++ Player.gd
@@ -2,19 +2,30 @@ extends KinematicBody2D
 
 
 var movimiento = Vector2()
-var cantidad = 200
+var cantidad = 400
 
+
+
+func _ready():
+	$AnimatedSprite.playing = true
+	pass
+	
 func _physics_process(delta):
 	move()
 	movimiento = move_and_slide(movimiento)
 	pass
 
 func move():
-	
+	movimiento.x = 0
+	movimiento.y = 0
 	if Input.is_action_pressed("ui_left"):
 		movimiento.x -= cantidad
+		$AnimatedSprite.flip_h = true
 	if Input.is_action_pressed("ui_right"):
 		movimiento.x += cantidad
+		$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed("ui_down"):
 		movimiento.y += cantidad
+	if Input.is_action_pressed("ui_up"):
+		movimiento.y -= cantidad
 	pass
